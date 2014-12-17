fad9cd022979ac108532483bedc3dbec6cf8d00e
kunal arora <kunalarora.135@gmail.com>
2014-12-17 12:44:57 +0530

    first commit-folder transfer

---
+++ one_1.5.0-RC1/compile.sh
@@ -0,0 +1,8 @@
+javac -Xlint:unchecked -extdirs lib/ core/*.java
+javac -Xlint:unchecked -extdirs lib/ movement/*.java
+javac -Xlint:unchecked -extdirs lib/ report/*.java
+javac -Xlint:unchecked -extdirs lib/ routing/*.java
+javac -Xlint:unchecked -extdirs lib/ gui/*.java
+javac -Xlint:unchecked -extdirs lib/ input/*.java
+javac -Xlint:unchecked -extdirs lib/ applications/*.java
+javac -Xlint:unchecked -extdirs lib/ interfaces/*.java
