5d8a591743e6ff648a71e02899f525dde3230f8a
Jigsaw2212 <agarwal.divyansh123@gmail.com>
2015-07-11 00:04:08 +0530

    KMeans working

--- one_1.5.0-RC1/compile.sh
+++ one_1.5.0-RC1/compile.sh
@@ -1,7 +1,7 @@
 javac -Xlint:unchecked -extdirs lib/ core/*.java
 javac -Xlint:unchecked -extdirs lib/ movement/*.java
 javac -Xlint:unchecked -extdirs lib/ report/*.java
-javac -Xlint:unchecked -extdirs lib/ routing/*.java
+javac -cp ".:/usr/share/java/jblas.jar:/usr/local/share/java/shogun.jar" -Xlint:unchecked -extdirs lib/ routing/*.java
 javac -Xlint:unchecked -extdirs lib/ gui/*.java
 javac -Xlint:unchecked -extdirs lib/ input/*.java
 javac -Xlint:unchecked -extdirs lib/ applications/*.java
--- one_1.5.0-RC1/one.sh
+++ one_1.5.0-RC1/one.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
-java -Xmx512M -cp .:lib/ECLA.jar:lib/DTNConsoleConnection.jar core.DTNSim $*
+java -Xmx512M -cp .:lib/ECLA.jar:lib/DTNConsoleConnection.jar:/usr/local/lib/gni:/usr/share/java/jblas.jar:/usr/local/share/java/shogun.jar:/usr/local/lib:/usr/local/share/java core.DTNSim $*
+
